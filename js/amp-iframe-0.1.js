83b2c04ae161826c00ca1e467acea3573bd3d325
Sai Charan Reddy Patlolla <scrpatlolla@charan.local>
2017-04-27 10:56:51 -0400

    Initial Commit from WebStrom

---
+++ js/amp-iframe-0.1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by scrpatlolla on 4/27/17.
+ */
+
+document.getElementById('.testingAMP').href="www.google.com";
+
---
+++ js/main.js
@@ -0,0 +1,1 @@
+document.getElementById('testingAMP').href= "#developer=1";
