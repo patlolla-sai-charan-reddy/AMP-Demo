903392dcac32ea5f07578320ce049dfbd21f40fc
Sai Charan Reddy Patlolla <scrpatlolla@charan.local>
2017-04-27 11:26:22 -0400

    fourth

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 document.getElementById('testingAMP').href= "#developer=1";
+
+var url = 'http://www.check.com/123';
+var id = url.substring(url.lastIndexOf('/') + 1);
+document.getElementById("sai").innerHTML = id + "  took from custom url";
